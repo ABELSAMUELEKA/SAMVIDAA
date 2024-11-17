0966b950f510576de8b6aab6a610a5d01df9c104
ABELSAMUELEKA <samuelabel255@gmail.com>
2024-11-17 23:57:09 +0100

    Add files via upload

---
+++ abel.py(ii).py
@@ -0,0 +1,36 @@
+def a():
+    # force = mass*acceleration
+    mass = float(input("what is the mass?"))
+    acceleration = float(input("what is the acceleration?"))
+    print(mass*acceleration)
+
+a();
+
+def b():
+    mass = float(input("what is the mass?"))
+    velocity = float(input("what is the acceleration?"))
+    print(mass*velocity)
+
+b();
+
+def c():
+    distance = float(input("what is distance?"))
+    time = float(input("how long was it?"))
+    print(distance/time)
+
+c();
+
+def d():
+    power = float(input("wha is the power?"))
+    time = float(input("how long did it take?"))
+    print(power*time)
+
+d();
+
+def e():
+    # young modulus = stress/strain
+    stress = float(input("force applied?"))
+    strain = float(input("extension made?"))
+    print(stress/strain)
+
+e();
